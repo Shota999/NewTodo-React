7de27f31bae4ea4aacb84ed6eaf85135db27d005
Shota999 <45195110+Shota999@users.noreply.github.com>
2022-11-02 17:02:03 +0400

    main

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './App.scss';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
+
+function App() {
+
+  const [ inputText, setInputText ] = useState("");
+  const [ todos, setTodos ] = useState([]);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo List</h1>
+        <Form 
+          todos={todos} 
+          setTodos={setTodos} 
+          setInputText={setInputText} 
+          inputText={inputText} />
+        <TodoList
+          setTodos={setTodos}
+          todos={todos} />
+    </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,21 @@
+import Todo from "./Todo";
+
+const TodoList = ({ todos , setTodos}) => {
+
+  return (
+    <div className="todo_container">
+      <ul className="todo_list">
+        {todos.map(todo => (
+            <Todo 
+                setTodos={setTodos}
+                todos={todos}
+                key={todo.id}
+                todo={todo}
+                text={todo.text} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default TodoList;
